0fc3288a5c26a053f15bddad01e4a67a21966d09
jax-01 <jrcastillo6@up.edu.ph>
2024-09-27 14:23:46 +0800

    Create a function that will handle the operation

--- script.js
+++ script.js
@@ -2,6 +2,25 @@ let num1;
 let num2;
 let operator;
 
+const operate = function(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      add(num1, num2);
+      break;
+    case "-":
+      subtract(num1, num);
+      break;
+    case "*":
+      multiply(num1, num2);
+      break;
+    case "/":
+      divide(num1, num2);
+      break;
+    default:
+      break;
+  }
+};
+
 const add = function(num1, num2) {
   return num1 + num2;
 };
