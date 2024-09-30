ef3863a259a3c83f517aed836a7eba026432d889
jax-01 <jrcastillo6@up.edu.ph>
2024-09-30 09:43:29 +0800

    Modify event handler for number buttons

--- script.js
+++ script.js
@@ -2,29 +2,13 @@ let num1;
 let num2;
 let operator;
 let displayValue = "0"; // variable that will store the current value shown in the display
+let shouldClearDisplay = false;
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll(".button");
 
 function updateDisplay() {
   display.textContent = displayValue;
 }
-
-buttons.forEach(button => {
-  // check if it is a number button or a decimal point
-  if (!isNaN(button.textContent) || button.textContent === ".") {
-    button.addEventListener("click", (event) => {
-      // get the digit
-      const buttonText = event.target.textContent;
-      if (displayValue === "0") {
-        displayValue = buttonText;
-      } else {
-        displayValue += buttonText;
-      }
-
-      updateDisplay();
-    });
-  }
-});
 
 const operate = function(operator, num1, num2) {
   switch (operator) {
@@ -57,3 +41,20 @@ const divide = function(num1, num2) {
   if (num2 == 0) throw new Error("Division by zero is not allowed");
   return num1 / num2;
 };
+
+buttons.forEach(button => {
+  // get the button clicked
+  const buttonText = button.textContent;
+  // check if it is a number button or a decimal point
+  if (!isNaN(buttonText) || buttonText === ".") {
+    button.addEventListener("click", () => {
+      if (shouldClearDisplay) {
+        displayValue = buttonText;
+        shouldClearDisplay = false;
+      } else {
+        displayValue = displayValue === "0" ? buttonText : displayValue + buttonText;
+      }
+      updateDisplay();
+    });
+  }
+});
